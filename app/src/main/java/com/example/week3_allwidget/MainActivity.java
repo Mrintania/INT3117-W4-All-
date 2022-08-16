286e1f3ca7b30eeedcbeb5b08e7fe619e6d7de3c
mrintania <95394889+Mrintania@users.noreply.github.com>
2022-08-16 11:29:08 +0700

    Add future on sw1 * spinner_txt

--- app/src/main/java/com/example/week3_allwidget/MainActivity.java
+++ app/src/main/java/com/example/week3_allwidget/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         Button btn_cal,btn_clr,btn_ent;
         SeekBar seekbar;
         Spinner spinner;
-        TextView tv_seek,tv_spin,tv_sw;
+        TextView tv_seek,tv_spin,tv_sw,tv_spin22;
         CheckBox cb_red,cb_blue,cb_green;
         RatingBar ratingBar;
         Switch sw1;
@@ -59,48 +59,15 @@ public class MainActivity extends AppCompatActivity {
         ratingBar = findViewById(R.id.ratingBar);
         sw1 = findViewById(R.id.switch1);
         tg_button = findViewById(R.id.toggleButton);
+
+        ///Alert
         AlertDialog.Builder alert_dialog = new AlertDialog.Builder(MainActivity.this);
         AlertDialog ald = alert_dialog.create();
 
-        btn_ent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tv_spin.setText(spinner.getSelectedItem().toString());
 
-                int seek_value = seekbar.getProgress();
-                tv_seek.setText(Integer.toString(seek_value));
-
-                if (cb_red.isChecked() && !cb_blue.isChecked() && !cb_green.isChecked()) {
-                    Toast.makeText(MainActivity.this, "red", Toast.LENGTH_SHORT).show();
-                }
-                else if (!cb_red.isChecked() && cb_blue.isChecked() && !cb_green.isChecked()) {
-                    Toast.makeText(MainActivity.this, "blue", Toast.LENGTH_SHORT).show();
-                }
-                else if (!cb_red.isChecked() && !cb_blue.isChecked() && cb_green.isChecked()) {
-                    Toast.makeText(MainActivity.this, "green", Toast.LENGTH_SHORT).show();
-                }
-                else if (!cb_red.isChecked() && !cb_blue.isChecked() && !cb_green.isChecked()) {
-                    Toast.makeText(MainActivity.this, "None Select", Toast.LENGTH_SHORT).show();
-                }
-                else if (cb_red.isChecked() && cb_blue.isChecked() && cb_green.isChecked()) {
-                    Toast.makeText(MainActivity.this, "U R select All", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        tg_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(tg_button.isChecked() == true){
-                    ald.setMessage("ON");
-                }
-                else {
-                    ald.setMessage("OFF");
-                }
-                ald.show();
-            }
-        });
-
+        ///SeekBar
+        int seek_value = seekbar.getProgress();
+        tv_seek.setText(Integer.toString(seek_value));
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -119,28 +86,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_ent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ///Spinner
+                tv_spin.setText(spinner.getSelectedItem().toString());
+
+                ///Switch
+                sw1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (sw1.isChecked()){
+                            tv_sw.setText("ON");
+                        }
+                        else {
+                            tv_sw.setText("OFF");
+                        }
+                    }
+                });
+
+
+                ///CheckBox
+                if (cb_red.isChecked() && !cb_blue.isChecked() && !cb_green.isChecked()) {
+                    Toast.makeText(MainActivity.this, "red", Toast.LENGTH_SHORT).show();
+                }
+                else if (!cb_red.isChecked() && cb_blue.isChecked() && !cb_green.isChecked()) {
+                    Toast.makeText(MainActivity.this, "blue", Toast.LENGTH_SHORT).show();
+                }
+                else if (!cb_red.isChecked() && !cb_blue.isChecked() && cb_green.isChecked()) {
+                    Toast.makeText(MainActivity.this, "green", Toast.LENGTH_SHORT).show();
+                }
+                else if (!cb_red.isChecked() && !cb_blue.isChecked() && !cb_green.isChecked()) {
+                    Toast.makeText(MainActivity.this, "None Select", Toast.LENGTH_SHORT).show();
+                }
+                else if (cb_red.isChecked() && cb_blue.isChecked() && cb_green.isChecked()) {
+                    Toast.makeText(MainActivity.this, "U R select All", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        ///ToggleButton
+        tg_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tg_button.isChecked() == true){
+                    ald.setMessage("ON");
+                }
+                else {
+                    ald.setMessage("OFF");
+                }
+                ald.show();
+            }
+        });
+
+        ///RatingBar
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-
                 Float val = ratingBar.getRating();
                 Toast.makeText(MainActivity.this, Float.toString(val),Toast.LENGTH_SHORT).show();
-
             }
         });
 
-        sw1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (sw1.isChecked()){
-                    tv_sw.setText("ON");
-                }
-                else {
-                    tv_sw.setText("OFF");
-                }
-            }
-        });
 
+
+        ///Calculation
         btn_cal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -167,6 +178,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ///ClearButton ClearData
         btn_clr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
